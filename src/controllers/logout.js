d1aab6c96ee73b6fb50fb57112ed621460a49d6c
Lucas Moura Corazim <lucasmouracorazim1@gmail.com>
2020-10-09 15:51:06 -0300

    Update logout.js

--- src/controllers/logout.js
+++ src/controllers/logout.js
@@ -4,17 +4,16 @@ const sessions = require('../data/sessions.json')
 module.exports = (req, res) => {
 
     const session = sessions.find(session => session.token === req.cookies.token)
-
-    if(session){
-
-        const persistSessions = sessions.filter(session => session.token !== req.cookies.token)
     
-        fs.writeFile(__rootname+'/src/data/sessions.json', JSON.stringify(persistSessions), err => err? console.error(err): 0)
-    
-        return res.clearCookie('token')
-                .status(200)
-                .json({'message': 'User logout'})
+    if(!session){
+        return res.status(404).json({'message': 'Not logged User'})
     }
 
-    return res.status(404).json({'message': 'Not logged User'})
+    const persistSessions = sessions.filter(session => session.token !== req.cookies.token)
+
+    fs.writeFile(__rootname+'/src/data/sessions.json', JSON.stringify(persistSessions), err => err? console.error(err): 0)
+
+    return res.clearCookie('token')
+            .status(200)
+            .json({'message': 'User logout'})
 }
