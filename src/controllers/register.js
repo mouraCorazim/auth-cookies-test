f5b19ff4865e11135127315e4867038bb267703e
Lucas Moura Corazim <lucasmouracorazim1@gmail.com>
2021-02-15 06:46:52 -0300

    Update register.js

--- src/controllers/register.js
+++ src/controllers/register.js
@@ -3,6 +3,31 @@ const fs = require('fs')
 const users = require('../data/users.json')
 const User = require('../models/User')
 
+function isValidCode(code) {
+    return code > 99 && code < 520
+}
+
+function isValidMessage(message){
+    return message != ""
+}
+
+function status(code, message){
+    return isValidMessage(message) && isValidCode(code)?
+            {'status': code, 'message': message} : {}
+}
+
+function registerUser(userEmail, userPassword, users, status){
+    const filePath = __rootname + '/src/data/users.json'
+    const data = [...users, User(userEmail, userPassword))]
+    const toWrite = JSON.stringify(data)
+    const callback = err => err? console.error(err): console.log("Registered user")
+
+    fs.writeFile(filePath, toWrite, callback)
+
+    return res.status(status.code) // 201
+              .json(status.message) // Registered user
+}
+
 module.exports = (req, res) => {
 
     const userEmail = req.body.email
@@ -17,10 +42,8 @@ module.exports = (req, res) => {
     if(userExists){
         return res.status(403).json({'message': 'User already exists'})
     }
-
-    users.push(User(userEmail, userPassword))
-
-    fs.writeFile(__rootname + '/src/data/users.json', JSON.stringify(users), err => err? console.error(err): 0)
-
-    return res.status(201).json({'message': 'User registered'})
+    
+    if(userEmail && userPassword && !userExists){
+        return registerUser(userEmail, userPassword, users, status(201, "Registered user"))
+    }
 }
