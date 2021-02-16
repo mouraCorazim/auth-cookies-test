6515ac715e1f7116ada6eceaeef0e062681cbba4
Lucas Moura Corazim <lucasmouracorazim1@gmail.com>
2021-02-16 03:31:49 -0300

    Update register.js

--- src/controllers/register.js
+++ src/controllers/register.js
@@ -18,25 +18,25 @@ function status(code, message){
 
 function Register(obj = {}){
     return {
-        'obj': obj,
-        'isValid': () => this.base && this.data && this.status
-        'base': (base) => base? Register({'base': base, ... this.obj}): Register(),
-        'of': (data) => data? Register({'data': data, ... this.obj}): Register(),
-        'status': (status) => status? Register({'status': status, ... this.obj}): Register(),
-        'chain': (fn) => fn && this.isValid()? fn(this.obj): Register()
+        'value': obj,
+        'isValid': () => this.value.base && this.value.data && this.value.status,
+        'base': (base) => base? Register({'base': base, ... this.value}): Register(),
+        'of': (data) => data? Register({'data': data, ... this.value}): Register(),
+        'status': (status) => status? Register({'status': status, ... this.value}): Register(),
+        'chain': (fn) => fn && this.isValid()? fn(this.value): Register()
     }
 }
 
 function registerUser(register){
     const filePath = __rootname + '/src/data/users.json'
-    const data = [... register.base, register.data]
+    const data = [... register.value.base, register.value.data]
     const toWrite = JSON.stringify(data)
     const callback = err => err? console.error(err): console.log("Registered user")
 
     fs.writeFile(filePath, toWrite, callback)
 
-    return res.status(register.status.code) // 201
-              .json(register.status.message) // Registered user
+    return res.status(register.value.status.code) // 201
+              .json(register.value.status.message) // Registered user
 }
 
 module.exports = (req, res) => {
