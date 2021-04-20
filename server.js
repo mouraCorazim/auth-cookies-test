a9b27ee84f15b64979b62aeb79505ac0c004fb66
Lucas Moura Corazim <lucasmouracorazim1@gmail.com>
2021-04-19 22:44:19 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const app = require('./src/app')
 const port = 8000
+const listeningMessage = 'listening on port: ' + port
+const cb = message => () => console.log(message)
 
 global.__rootname = __dirname
 
-app.listen(port, () => console.log('listening on port: ' + port))
+app.listen(port, cb(listeningMessage))
